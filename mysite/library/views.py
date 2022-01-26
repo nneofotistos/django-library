f6eea1cac0cd3c0c8596e5176eb076e1005b296c
nneofotistos <n_neofotistos@yahoo.com>
2022-01-25 18:02:10 -0600

    auth

---
+++ mysite/library/views.py
@@ -0,0 +1,60 @@
+from django.shortcuts import render, redirect
+# from .models import Book
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.http import HttpResponseRedirect, HttpResponse
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
+
+
+
+def login_view(request):
+    if request.method == 'POST':
+        # try to log user in
+        form = AuthenticationForm(request, request.POST)
+        if form.is_valid():
+            u = form.cleaned_data['username']
+            p = form.cleaned_data['password']
+            user = authenticate(username = u, password = p)
+            if user is not None:
+                if user.is_active:
+                    login(request, user) # log the user in by creating a session
+                    return redirect('/user/'+u)
+                else:
+                    print('The account has been disabled.')
+                    return redirect('/login')
+        else:
+            print('The username and/or password is incorrect.')
+            return redirect('/login')
+    else: # it was a GET request so send the empty login form
+        form = AuthenticationForm()
+        return render(request, 'login.html', {'form': form})
+
+def logout_view(request):
+    logout(request)
+    return redirect('/')
+
+def signup_view(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('/user/'+str(user))
+        else:
+            return HttpResponse('<h1>Try Again</h1>')
+    else:
+        form = UserCreationForm()
+        return render(request, 'signup.html', {'form': form})
+
+@login_required
+def profile(request, username):
+    user = User.objects.get(username=username)
+    return render(request, 'profile.html', {'username': username})
+
+
+def index(request):
+    return HttpResponse("Hello, world. You're at the index.")
+# Create your views here.
