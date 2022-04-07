162c98f73761343144caf765fe69e238d86ef5d0
nneofotistos <n_neofotistos@yahoo.com>
2022-04-07 14:06:10 -0500

    host later

--- mysite/library/admin.py
+++ mysite/library/admin.py
@@ -17,5 +17,6 @@ class UserAdmin(BaseUserAdmin):
 admin.site.unregister(User)
 admin.site.register(User, UserAdmin)
 admin.site.register(Book)
+admin.site.register(LoanedBook)
 
 # Register your models here.
